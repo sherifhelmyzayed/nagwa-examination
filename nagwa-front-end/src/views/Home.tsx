359c747762ddccf8172acd4639923b8946ff9c1a
sherifhelmyzayed <sherifhelmyzayed@gmail.com>
2022-12-25 00:02:06 +0200

    feat: added tailwind css config and react router

---
+++ nagwa-front-end/src/views/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div className='mb-10 text-5xl'>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+            <div>Home</div>
+        </>
+    )
+}
+
+export default Home
