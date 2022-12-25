a3e16c89b8bb6e548afb79fccad23b65dce09ddb
sherif zayed <sherifhelmyzayed@gmail.com>
2022-12-25 23:28:45 +0200

    feat: added questions and its page layout

---
+++ nagwa-front-end/src/components/AnswerButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AnswerButton = (props: any) => {
+
+    return (
+        <div className="col-span-1 mx-auto" onClick={() => props.clickHandler(props.text)}>
+            <button className={`
+            w-44  bg-transparent hover:bg-gray-500 py-2 px-4 rounded
+             text-gray-300 font-semibold hover:text-white
+              border ${props.selectedAns === props.text ? "border-lime-300" : "border-gray-300"} hover:border-gray-600 focus:outline-none
+            `}>
+                {props.text}
+            </button>
+        </div>
+    )
+}
+
+export default AnswerButton
--- nagwa-front-end/src/views/Home.tsx
+++ nagwa-front-end/src/views/Home.tsx
@@ -1,22 +1,40 @@
 import React, { useState } from 'react'
+import AnswerButton from '../components/AnswerButton';
 
 const Home = (props: any) => {
-    const [quesIndex, setQuesIndex] = useState(0);
+    const [quesIndex, setQuesIndex] = useState<number>(0);
+    const [selectedAns, setSelectedAns] = useState<string | null>(null);
 
-    const questions = props.questions
+    const questions = props.questions;
+
+    const selectAnswer = (ans: string) => {
+
+        // if there is no selected answer. Check for the correct answer and play animation
+        if (!selectedAns) {
+            console.log(questions[quesIndex].pos === ans);
+        }
+
+        setSelectedAns(ans);
+
+    }
+
+
     return (
         (questions) ? (
 
             <>
                 <div className=" mx-auto grid grid-cols-3 gap-1 w-screen h-screen ">
-                    <div className="col-span-1">
+                    <div className="col-span-1 bg-white">
                         <div className="progress">Progress Circle</div>
-                        <div className="questionNumbers">
-                            Numbers from 1-10
+
+                        <div>
+                            Questions Circles
                         </div>
-                        <div className="timer"></div>
+                        <div className="timer">
+                            Timer
+                        </div>
                     </div>
-                    <div className="col-span-2 flex flex-col justify-between items-center h-full w-full">
+                    <div className="col-span-2 flex flex-col justify-between items-center h-full w-full bg-gray-200">
                         <div className="flex justify-between">
                             <div>
                                 <h2>Category: <span>words</span></h2>
@@ -25,12 +43,21 @@ const Home = (props: any) => {
                             <div>flag</div>
                         </div>
                         <div className="Question">
-                            {questions[quesIndex].word}
-                            <div className="answers">
-                                <button>adverb</button>
-                                <button>verb</button>
-                                <button>noun</button>
-                                <button>adjective</button>
+                            <div className='text-gray-500 text-center mb-10'>
+                                <p>
+
+                                    Question no. {quesIndex + 1} / {questions.length}
+                                </p>
+                                <h2 className='text-lg font-semibold'>
+                                    {questions[quesIndex].word}
+
+                                </h2>
+                            </div>
+                            <div className="grid grid-cols-2 gap-4 w-96 ">
+                                <AnswerButton selectedAns={selectedAns} clickHandler={selectAnswer} text={"adverb"} />
+                                <AnswerButton selectedAns={selectedAns} clickHandler={selectAnswer} text={"verb"} />
+                                <AnswerButton selectedAns={selectedAns} clickHandler={selectAnswer} text={"noun"} />
+                                <AnswerButton selectedAns={selectedAns} clickHandler={selectAnswer} text={"adjective"} />
                             </div>
                         </div>
                         <div className="actionButtons">
