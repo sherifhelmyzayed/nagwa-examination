1473513aea3450c0499b43dd090c89248294ab31
sherif zayed <sherifhelmyzayed@gmail.com>
2022-12-25 20:10:47 +0200

    feat: imported static questions

--- nagwa-front-end/src/App.tsx
+++ nagwa-front-end/src/App.tsx
@@ -1,17 +1,31 @@
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import Result from './views/Result'
 import './App.css'
-// import './styles/Index.scss'
+// import './styles/Index.scss';
+import Data from './shared/TestData.json'
+
+
 
 function App() {
+
+  const [questions, setQuestions] = useState<any>(null);
+
+
+  // for axios call get questions endpoint
+  useEffect(() => {
+    setQuestions(Data.wordList)
+  }, [])
+
+
 
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home questions={questions} />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
--- nagwa-front-end/src/views/Home.tsx
+++ nagwa-front-end/src/views/Home.tsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Home = () => {
+const Home = (props: any) => {
+    const [quesIndex, setQuesIndex] = useState(0);
+
+    const questions = props.questions
     return (
-        <>
-            <div className='mb-10 text-5xl'>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-            <div>Home</div>
-        </>
+        (questions) ? (
+
+            <>
+                <div className=" mx-auto grid grid-cols-3 gap-1 w-screen h-screen ">
+                    <div className="col-span-1">
+                        <div className="progress">Progress Circle</div>
+                        <div className="questionNumbers">
+                            Numbers from 1-10
+                        </div>
+                        <div className="timer"></div>
+                    </div>
+                    <div className="col-span-2 flex flex-col justify-between items-center h-full w-full">
+                        <div className="flex justify-between">
+                            <div>
+                                <h2>Category: <span>words</span></h2>
+                            </div>
+                            <div>Profile</div>
+                            <div>flag</div>
+                        </div>
+                        <div className="Question">
+                            {questions[quesIndex].word}
+                            <div className="answers">
+                                <button>adverb</button>
+                                <button>verb</button>
+                                <button>noun</button>
+                                <button>adjective</button>
+                            </div>
+                        </div>
+                        <div className="actionButtons">
+                            <button>
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        ) : null
     )
 }
 
