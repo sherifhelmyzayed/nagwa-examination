255b233e399dadd0175aad4ddb74ba166b164e1d
sherif zayed <sherifhelmyzayed@gmail.com>
2022-12-27 17:30:53 +0200

    feat: Word endpoint works

--- nagwa-back-end/src/app.ts
+++ nagwa-back-end/src/app.ts
@@ -1,13 +1,41 @@
 import express from 'express';
+import fs from 'fs';
+
 const app = express();
 const port = 4000;
+
+interface QuestionInterface {
+    id: number;
+    word: string;
+    pos: string;
+    answer: string | null;
+}
+
+interface QuestionsInterface extends Array<QuestionInterface> { }
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/word', (req, res) => {
-    res.send('Word enpoint works!');
+app.get('/word', async (req, res) => {
+
+    try {
+        const data = await fs.promises.readFile(__dirname + '/testData.json', 'utf8')
+        const wordList = JSON.parse(data).wordList;
+        const shuffle = (array: QuestionsInterface) => {
+            return array.sort(() => 0.5 - Math.random());
+        };
+        const shuffledList = shuffle(wordList).slice(0, 10)
+        res.send(shuffledList)
+    }
+    catch (err) {
+        console.log(err)
+        res.send(err)
+    }
+
+
 });
 
 app.get('/rank', (req, res) => {
